291db04f68f59dd7f6f897a93e4acc1da92471b0
Murad Abdullah <mabdullah.ust@outlook.com>
2026-08-13 01:07:42 +0800

    clean up the Pretransform_generator_file::load_pretransform_file

--- src/Tools/Code/Polar/Pretransform_generator/Pretransform_generator_file.cpp
+++ src/Tools/Code/Polar/Pretransform_generator/Pretransform_generator_file.cpp
@@ -1,40 +1,41 @@
 #include <fstream>
 #include <sstream>
 #include <streampu.hpp>
+#include <sys/types.h>
 
 #include "Tools/Code/Polar/Pretransform_generator/Pretransform_generator_file.hpp"
 #include "Tools/general_utils.h"
 
 using namespace aff3ct::tools;
 
-Pretransform_generator_file ::Pretransform_generator_file(const int K, const int N, const std::string& filename)
+Pretransform_generator_file::Pretransform_generator_file(const int K, const int N, const std::string& filename)
   : Pretransform_generator(K, N)
   , filename(filename)
 {
 }
 
-Pretransform_generator_file ::Pretransform_generator_file(const int K, const int N)
+Pretransform_generator_file::Pretransform_generator_file(const int K, const int N)
   : Pretransform_generator(K, N)
   , filename("")
 {
 }
 
 Pretransform_generator_file*
-Pretransform_generator_file ::clone() const
+Pretransform_generator_file::clone() const
 {
     auto t = new Pretransform_generator_file(*this);
     return t;
 }
 
 void
-Pretransform_generator_file ::evaluate()
+Pretransform_generator_file::evaluate()
 {
     if (!load_pretransform_file(this->filename, this->preTransform, this->info_bits_loc))
         throw spu::tools::invalid_argument(__FILE__, __LINE__, __func__, "'" + filename + "' file does not exist.");
 }
 
 bool
-Pretransform_generator_file ::load_channels_file(const std::string& filename, std::vector<uint32_t>& best_channels)
+Pretransform_generator_file::load_channels_file(const std::string& filename, std::vector<uint32_t>& best_channels)
 {
     std::ifstream in_code(filename.c_str());
 
@@ -84,39 +85,60 @@ Pretransform_generator_file::load_pretransform_file(const std::string& filename,
     if (in_pretransform.is_open())
     {
         std::string line;
-        // int k = 0;
+        pretransform.clear();
         while (!in_pretransform.eof())
         {
 
             tools::getline(in_pretransform, line);
 
             auto values = tools::split(line);
-            if (values.size() > 0)
+            if (values.empty()) continue;
+
+            uint32_t current_key;
+            try
             {
-                unsigned int current_key = std::stoi(values.at(0));
-                // std::cout << "size: " << current_key << std::endl;
-                // if (k < this->K) info_bits_loc[k++] = current_key;
-                // this->pre_tranform[current_key] = std::vector<unsigned int>(values.size() - 1);
-                pretransform[current_key] = std::vector<uint32_t>(values.size() - 1);
-                int idx = 0;
-                for (auto it = values.begin() + 1; it != values.end(); ++it)
+                current_key = std::stoi(values.at(0));
+            }
+            catch (const std::exception& e)
+            {
+                throw spu::tools::runtime_error(
+                  __FILE__, __LINE__, __func__, "Failed to convert string to uint32_t: " + std::string(e.what()));
+            }
+
+            if ((int)current_key >= this->N)
+            {
+                std::stringstream message;
+                message << "Pre-transform key is out of bounds: " + std::to_string(current_key) +
+                             " is greater than or equal to N: " + std::to_string(this->N);
+                throw spu::tools::runtime_error(__FILE__, __LINE__, __func__, message.str());
+            }
+
+            pretransform[current_key] = std::vector<uint32_t>(values.size() - 1);
+            int idx = 0;
+            for (auto it = values.begin() + 1; it != values.end(); ++it)
+            {
+                uint32_t index;
+                try
                 {
-                    // pretransform[current_key].push_back(std::stoi(*it));
-                    pretransform[current_key][idx++] = std::stoi(*it);
-                    // this->pre_tranform[current_key].at(idx++) = std::stoi(*it);
-                    // std::cout << *it << ",";
+                    index = (uint32_t)std::stoi(*it);
                 }
-                // std::cout << std::endl;
+                catch (const std::exception& e)
+                {
+                    throw spu::tools::runtime_error(
+                      __FILE__, __LINE__, __func__, "Failed to convert string to uint32_t: " + std::string(e.what()));
+                }
+
+                if ((int)index >= this->K)
+                {
+                    std::stringstream message;
+                    message << "Pre-transform index is out of bounds: " + std::to_string(index) +
+                                 " is greater than or equal to K: " + std::to_string(this->K);
+                    throw spu::tools::runtime_error(__FILE__, __LINE__, __func__, message.str());
+                }
+                pretransform[current_key][idx++] = index;
             }
         }
         in_pretransform.close();
-        // for (const auto& vk : pretransform)
-        // {
-        //     std::cout << "Key: " << vk.first << ":";
-        //     for (const auto& v : vk.second)
-        //         std::cout << v << ",";
-        //     std::cout << std::endl;
-        // }
 
         return true;
     }
@@ -125,7 +147,7 @@ Pretransform_generator_file::load_pretransform_file(const std::string& filename,
 }
 
 void
-Pretransform_generator_file ::check_noise()
+Pretransform_generator_file::check_noise()
 {
     Pretransform_generator::check_noise();
 }
